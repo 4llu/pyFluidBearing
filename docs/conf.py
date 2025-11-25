dff9855d7cc58eac5988247fc97e168b8239062b
Elmo Laine <elmo.laine@aalto.fi>
2025-11-25 12:56:47 +0200

    Fix theme for GitHub Pages

--- docs/conf.py
+++ docs/conf.py
@@ -1,26 +1,18 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../src'))
 
+# -- Project information -----------------------------------------------------
 project = 'pyFluidBearing'
 copyright = '2025, Aleksanteri Hämäläinen, Oliver Häggman, Huy Nguyen, Elmo Laine'
 author = 'Aleksanteri Hämäläinen, Oliver Häggman, Huy Nguyen, Elmo Laine'
-release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
     'sphinx_rtd_theme',
 ]
@@ -28,10 +20,19 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# -- HTML output -------------------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
+
+html_baseurl = "https://4llu.github.io/pyFluidBearing/"
+
+# Use relative URLs for all static files (critical for GitHub Pages)
 html_static_path = ['_static']
+html_css_files = []
+html_js_files = []
+
+html_copy_source = True
+html_show_sourcelink = True
+html_add_permalinks = True
+
+html_use_index = True
+html_domain_indices = True
