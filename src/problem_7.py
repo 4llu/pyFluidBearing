5585e1ffe9adc19e156b35fa5ec524824e5b9b40
oliverhaggman <oliver.haggman@aalto.fi>
2025-11-25 11:13:55 +0200

    Update problem_7.py
    Updates (more functions added)

--- src/problem_7.py
+++ src/problem_7.py
@@ -158,11 +158,31 @@ class Bearing:
         self.Gyro = np.zeros((4, 4))
 
 
-class System:
-    def __init__(self, n_nodes, shafts, disks, bearings, ndof_per_node=4):
+class RotorSystem:
+    def __init__(self, bearings):
+        # Material and geometry
+        rho = 7810.0
+        E = 211e9
+        v = 0.3
+
+        r_disk = 0.25 / 2.0
+        l_disk = 0.04
+        r_shaft = 0.025 / 2.0
+        l_shaft = 0.25
+
+        n_nodes = 7
+
+        shafts = []
+        for i in range(n_nodes - 1):
+            el = Shaft(n1=i, n2=i + 1, L=l_shaft, R=r_shaft, E=E, rho=rho, v=v)
+            shafts.append(el)
+
+        # Disk at node 6
+        disk_1 = Disk(node=6, R=r_disk, l=l_disk, rho=rho)
+        disks = [disk_1]
         self.n_nodes = n_nodes
-        self.ndof_per_node = ndof_per_node
-        self.Ndof = n_nodes * ndof_per_node
+        self.ndof_per_node = 4
+        self.Ndof = n_nodes * self.ndof_per_node
         self.shafts = shafts
         self.disks = disks
         self.bearings = bearings
@@ -182,10 +202,10 @@ class System:
                     self.G[base + i, base + j] += s.Gyro[i, j]
                     self.K[base + i, base + j] += s.K[i, j]
                     self.C[base + i, base + j] += s.C[i, j]
-        print(np.sum(self.K))
+
         # assemble disks
         for d in self.disks:
-            base = 24
+            base = d.node * self.ndof_per_node
             for i in range(4):
                 for j in range(4):
                     self.M[base + i, base + j] += d.M[i, j]
@@ -211,92 +231,78 @@ class System:
             (-Minv @ K_mat, -Minv @ (self.C + Omega * self.G)), axis=1
         )
         A = np.concatenate((A_top, A_bottom), axis=0)
-        # print(A)
-        # print(np.sum(K_mat))
-        # print(np.sum(self.M[::4]))
         return A
 
     def eigenvalues(self, Omega=0.0):
-        """
-        Compute eigenvalues (lambda) of the state-space matrix. We return eigenvalues (complex).
-        """
         A = self.state_space_matrix(Omega=Omega)
         eigvals, eigvecs = LA.eig(A)
         eigvals = sorted(eigvals, key=np.abs)
         return eigvals, eigvecs
 
+    def campbell(self, speeds):
+        eig_1 = np.zeros_like(speeds)
+        eig_2 = np.zeros_like(speeds)
+        eig_3 = np.zeros_like(speeds)
+        eig_4 = np.zeros_like(speeds)
+        eig_5 = np.zeros_like(speeds)
+        eig_6 = np.zeros_like(speeds)
+
+        for i, speed in enumerate(speeds):
+            eig, _ = rotor.eigenvalues(Omega=speed / 60 * 2 * np.pi)
+            eig_1[i] = np.abs(eig[0])
+            eig_2[i] = np.abs(eig[2])
+            eig_3[i] = np.abs(eig[4])
+            eig_4[i] = np.abs(eig[6])
+            eig_5[i] = np.abs(eig[8])
+            eig_6[i] = np.abs(eig[10])
+
+        fig, ax = plt.subplots(1, 1, figsize=(6, 5))
+
+        ax.plot(speeds, speeds / 60, "b--", label="Rotor speed")
+        ax.plot(speeds, eig_1 / (2 * np.pi), label="First")
+        ax.plot(speeds, eig_2 / (2 * np.pi), label="Second")
+        ax.plot(speeds, eig_3 / (2 * np.pi), label="Third")
+        ax.plot(speeds, eig_4 / (2 * np.pi), label="Fourth")
+        ax.plot(speeds, eig_5 / (2 * np.pi), label="Fifth")
+        ax.plot(speeds, eig_6 / (2 * np.pi), label="Sixth")
+        ax.tick_params(labelsize=11)
+
+        fig.legend(loc="upper center", ncol=3, bbox_to_anchor=(0.5, 1), fontsize=11)
+        ax.set_xlabel("Speed (RPM)", fontsize=11)
+        ax.set_ylabel("Natural frequency (Hz)", fontsize=11)
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.83)
+        plt.show()
+
+    def print_eigs(self):
+
+        eig_0, _ = self.eigenvalues(Omega=0.0)
+        print("Eigenvalues at Omega = 0:")
+        for i in range(8):
+            print(f"{np.real(eig_0[i]):.2f} {np.imag(eig_0[i]):.2f}")
+
+        rpm = 1000.0
+        Omega = rpm / 60.0 * 2.0 * np.pi
+        eig_1000, _ = self.eigenvalues(Omega=Omega)
+        print("\nEigenvalues at Omega = 1000 rpm:")
+        for i in range(8):
+            print(f"{np.real(eig_1000[i]):.2f} {np.imag(eig_1000[i]):.2f}")
+
+        rpm = 3000.0
+        Omega = rpm / 60.0 * 2.0 * np.pi
+        eig_3000, _ = self.eigenvalues(Omega=Omega)
+        print("\nEigenvalues at Omega = 3000 rpm:")
+        for i in range(8):
+            print(f"{np.real(eig_3000[i]):.2f} {np.imag(eig_3000[i]):.2f}")
+
 
 if __name__ == "__main__":
-    # Material and geometry
-    rho = 7810.0
-    E = 211e9
-    v = 0.3
-
-    r_disk = 0.25 / 2.0
-    l_disk = 0.04
-    r_shaft = 0.025 / 2.0
-    l_shaft = 0.25
-
-    n_nodes = 7
-
-    shafts = []
-    for i in range(n_nodes - 1):
-        el = Shaft(n1=i, n2=i + 1, L=l_shaft, R=r_shaft, E=E, rho=rho, v=v)
-        shafts.append(el)
-
-    # Disk at node 6
-    disk_1 = Disk(node=6, R=r_disk, l=l_disk, rho=rho)
-    disks = [disk_1]
-
     # Bearings at node 0 and 4
     bearing_1 = Bearing(node=0, kxx=0.2e6, kyy=0.4e6, cxx=0.0, cyy=0.0)
     bearing_2 = Bearing(node=4, kxx=0.2e6, kyy=0.4e6, cxx=0.0, cyy=0.0)
     bearings = [bearing_1, bearing_2]
 
-    rotor = System(n_nodes=n_nodes, shafts=shafts, disks=disks, bearings=bearings)
-
+    rotor = RotorSystem(bearings=bearings)
     speeds = np.linspace(0, 3000, 1000)
-    eig_1 = np.zeros_like(speeds)
-    eig_2 = np.zeros_like(speeds)
-    eig_3 = np.zeros_like(speeds)
-    eig_4 = np.zeros_like(speeds)
-    eig_5 = np.zeros_like(speeds)
-    eig_6 = np.zeros_like(speeds)
-
-    for i, speed in enumerate(speeds):
-        eig, _ = rotor.eigenvalues(Omega=speed / 60 * 2 * np.pi)
-        eig_1[i] = np.abs(eig[0])
-        eig_2[i] = np.abs(eig[2])
-        eig_3[i] = np.abs(eig[4])
-        eig_4[i] = np.abs(eig[6])
-        eig_5[i] = np.abs(eig[8])
-        eig_6[i] = np.abs(eig[10])
-
-    plt.figure(figsize=(6, 4))
-
-    plt.plot(speeds, speeds / 60, "b--", label="Rotor speed")
-    plt.plot(speeds, eig_1 / (2 * np.pi), label="First")
-    plt.plot(speeds, eig_2 / (2 * np.pi), label="Second")
-    plt.plot(speeds, eig_3 / (2 * np.pi), label="Third")
-    plt.plot(speeds, eig_4 / (2 * np.pi), label="Fourth")
-    plt.plot(speeds, eig_5 / (2 * np.pi), label="Fifth")
-    plt.plot(speeds, eig_6 / (2 * np.pi), label="Sixth")
-
-    plt.legend()
-    plt.xlabel("Speed (RPM)")
-    plt.ylabel("Natural frequency (Hz)")
-    plt.show()
-
-    eig_0, _ = rotor.eigenvalues(Omega=0.0)
-    eig_0 = sorted(eig_0, key=np.abs)
-    print("Eigenvalues at Omega = 0:")
-    for i in range(8):
-        print(eig_0[i])
-
-    rpm = 1000.0
-    Omega = rpm / 60.0 * 2.0 * np.pi
-    eig_1000, _ = rotor.eigenvalues(Omega=Omega)
-    eig_1000 = sorted(eig_1000, key=np.abs)
-    print("\nEigenvalues at Omega = 1000 rpm:")
-    for i in range(8):
-        print(eig_1000[i])
+    rotor.campbell(speeds)
+    rotor.print_eigs()
